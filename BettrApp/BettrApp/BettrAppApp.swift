9c7e261229d47330a802b7bded2950c6e6ccc7fb
Finbar McCarron <115332264+finnymc11@users.noreply.github.com>
2025-06-17 13:49:38 -0400

    Create project template

---
+++ BettrApp/BettrApp/BettrAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BettrAppApp.swift
+//  BettrApp
+//
+//  Created by Finbar McCarron on 6/17/25.
+//
+
+import SwiftUI
+
+@main
+struct BettrAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ app.swift
@@ -0,0 +1,4 @@
+var apples = 23
+apples += 1
+
+let hello: String = "hi mom"
