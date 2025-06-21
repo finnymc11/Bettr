875e1d5fe6ccd5d9a9bc77d1dabc47b9e4ae9db1
Finbar McCarron <115332264+finnymc11@users.noreply.github.com>
2025-06-21 16:56:04 -0400

    Initial Commit

---
+++ Bettr/BettrApp.swift
@@ -0,0 +1,17 @@
+//
+//  BettrApp.swift
+//  Bettr
+//
+//  Created by Finbar McCarron on 6/21/25.
+//
+
+import SwiftUI
+
+@main
+struct BettrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bettr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bettr
+//
+//  Created by Finbar McCarron on 6/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
