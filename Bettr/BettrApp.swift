7e4c945f054e29d472565b0e060ea32ffcb86ddc
Finbar McCarron <115332264+finnymc11@users.noreply.github.com>
2025-06-26 10:17:39 -0400

    Update BettrApp.swift

--- Bettr/BettrApp.swift
+++ Bettr/BettrApp.swift
@@ -21,58 +21,46 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 struct BettrApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @State private var currentScreen: Screen = .splash
-
-        
     @StateObject var auth = fireAuth()
     
-    
     enum Screen {
-        case splash, signUp, home, createAccount, settings,screenTime
+        case splash, signUp, home, accountCreation, settings, screenTime
     }
     
     var body: some Scene {
         WindowGroup {
             ZStack{
                 Color.black.ignoresSafeArea()
-
-                if auth.user != nil {
-                    Home()
-                } else {
-                    switch currentScreen {
-                    case .splash:
-                        SplashView()
-                            .onAppear {
-                                if auth.user != nil {
-                                    currentScreen = .home
-                                } else {
-                                    DispatchQueue.main.asyncAfter(deadline: .now() + 9) {
-                                        withAnimation {
-                                            currentScreen = .signUp
-                                        }
+                switch currentScreen {
+                case .splash:
+                    SplashView()
+                        .onAppear {
+                            if auth.user != nil {
+                                currentScreen = .home
+                            } else {
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 9) {
+                                    withAnimation {
+                                        currentScreen = .signUp
                                     }
                                 }
                             }
-                            .transition(.opacity)
-
-                    case .signUp:
-                        SignUp(currentScreen: $currentScreen)
-                            .environmentObject(auth)
-                            .transition(.opacity)
-
-                    case .createAccount:
-                        AccountCreation(currentScreen: $currentScreen)
-                            .transition(.opacity)
-
-                    case .screenTime:
-                        ScreenTime(currentScreen: $currentScreen)
-                            .transition(.opacity)
-
-                    case .home:
-                        Home()
-
-                    case .settings:
-                        SettingsView(currentScreen: $currentScreen)
-                    }
+                        }
+                        .transition(.opacity)
+                case .signUp:
+                    SignUp(currentScreen: $currentScreen)
+                        //.environmentObject(auth)
+                        .transition(.opacity)
+                case .accountCreation:
+                    AccountCreation(currentScreen: $currentScreen)
+                        .transition(.opacity)
+                case .screenTime:
+                    ScreenTime(currentScreen: $currentScreen)
+                        .transition(.opacity)
+                case .home:
+                    Home()
+                case .settings:
+                    SettingsView(currentScreen: $currentScreen)
+                }
                 }
             }
             .animation(.easeIn(duration: 0.5), value: currentScreen)
