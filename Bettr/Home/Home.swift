35fcff11ae02d0cc64393cf40602f3df283292e3
Finbar McCarron <115332264+finnymc11@users.noreply.github.com>
2025-06-26 10:27:54 -0400

    Update Home.swift

--- Bettr/Home/Home.swift
+++ Bettr/Home/Home.swift
@@ -25,10 +25,6 @@ struct Home: View {
 }
 
 struct homeView: View {
-
-    @State private var currentScreen: BettrApp.Screen = .home
-
-
     init() {
         let appearance = UINavigationBarAppearance()
         //appearance.configureWithOpaqueBackground() // makes it solid
@@ -44,10 +40,6 @@ struct homeView: View {
         UINavigationBar.appearance().standardAppearance = appearance
         UINavigationBar.appearance().scrollEdgeAppearance = appearance
     }
-
-//    @Binding var currentScreen: BettrApp.Screen
-
-
     var body: some View {
         NavigationStack {
             VStack(spacing: 0) {
@@ -65,9 +57,6 @@ struct homeView: View {
                         .font(.system(size: 20))
                 }
                 Spacer()
-
-
-
                 // Bottom overlay
                 VStack {
                     Text("You're spending about 99% less screen time than the average person")
@@ -79,20 +68,14 @@ struct homeView: View {
                         .font(.system(size: 20))
                         .padding(.bottom, 50)
                 }
-
             }.cStyle1()
             .navigationTitle("Bettr.")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
-
                     NavigationLink(destination: SettingsView(currentScreen: $currentScreen)) {
                         Image(systemName: "gear")
                             .font(.system(size: 20))
-                        //                    NavigationLink(destination: SettingsView()) {
-                        //
-                        //                        
-                        //                    }
                     }
                 }
             }
